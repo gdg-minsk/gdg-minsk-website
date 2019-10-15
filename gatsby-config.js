6fbbeb3a23743d87d3576a84fc192b1d2902ef9c
Ilya Zaprutski <ilya.zaprutski@gmail.com>
2019-10-15 15:44:14 +0300

    update site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     siteMetadata: {
-        title: 'GDG Minsk Test cache',
+        title: 'GDG Minsk',
         description: 'GDG Description.',
         author: 'gdg',
         menuItems: [
