fa9ed25ab2ccda06ec0f636865d21969acdf85b8
Ilya Zaprutski <ilya.zaprutski@gmail.com>
2020-03-07 23:36:40 +0300

    add preconnect

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,14 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react';
 
-// You can delete this file if you're not using it
+export const onRenderBody = ({ setHeadComponents }) => {
+    setHeadComponents([
+        <link rel="preconnect" key="dns-preconnect-google-font" href="https://fonts.gstatic.com" />,
+        <link rel="dns-prefetch" key="dns-prefetch-google-font" href="https://fonts.gstatic.com" />,
+
+        <link rel="preconnect" key="dns-preconnect-segment" href="https://cdn.segment.com" />,
+        <link rel="dns-prefetch" key="dns-prefetch-segment" href="https://cdn.segment.com" />,
+
+        <link rel="preconnect" key="dns-preconnect-google-analytics" href="https://www.google-analytics.com" />,
+        <link rel="dns-prefetch" key="dns-prefetch-google-analytics" href="https://www.google-analytics.com" />,
+    ]);
+};
