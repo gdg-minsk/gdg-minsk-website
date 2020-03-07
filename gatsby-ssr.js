def1ceea59bb0079a679099f5155a11585401d72
Ilya Zaprutski <ilya.zaprutski@gmail.com>
2020-03-08 00:00:16 +0300

    add segment api preconnect

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,6 +8,9 @@ export const onRenderBody = ({ setHeadComponents }) => {
         <link rel="preconnect" key="dns-preconnect-segment" href="https://cdn.segment.com" />,
         <link rel="dns-prefetch" key="dns-prefetch-segment" href="https://cdn.segment.com" />,
 
+        <link rel="preconnect" key="dns-preconnect-segment-api" href="https://api.segment.io" />,
+        <link rel="dns-prefetch" key="dns-prefetch-segment-api" href="https://api.segment.io" />,
+
         <link rel="preconnect" key="dns-preconnect-google-analytics" href="https://www.google-analytics.com" />,
         <link rel="dns-prefetch" key="dns-prefetch-google-analytics" href="https://www.google-analytics.com" />,
     ]);
