a021a4df1d583e0d3ea87d2c0690b7d6335570f1
Ilya Zaprutski <ilya.zaprutski@gmail.com>
2020-03-08 11:52:47 +0300

    delete precconect for fonts

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 export const onRenderBody = ({ setHeadComponents }) => {
     setHeadComponents([
-        <link rel="preconnect" key="dns-preconnect-google-font" href="https://fonts.gstatic.com" />,
-        <link rel="dns-prefetch" key="dns-prefetch-google-font" href="https://fonts.gstatic.com" />,
-
         <link rel="preconnect" key="dns-preconnect-segment" href="https://cdn.segment.com" />,
         <link rel="dns-prefetch" key="dns-prefetch-segment" href="https://cdn.segment.com" />,
 
