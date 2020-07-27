3114e031c7f43d5b69a33468f4e0a808ed8368d6
ArtemKoroteev <artem_koroteev@epam.com>
2020-07-27 15:04:08 +0300

    Fix lintr errors

--- src/components/shared/event-card/event-card.tsx
+++ src/components/shared/event-card/event-card.tsx
@@ -41,10 +41,10 @@ const EventCard = ({ imageUrl, imageTitle, title, description, date, speakers })
                         className={classes.avatar}
                     />
                 ) : (
-                        <Avatar className={classes.avatar}>
-                            {`${speaker.firstName.charAt(0)} ${speaker.lastName.charAt(0)}`}
-                        </Avatar>
-                    )}
+                    <Avatar className={classes.avatar}>
+                        {`${speaker.firstName.charAt(0)} ${speaker.lastName.charAt(0)}`}
+                    </Avatar>
+                )}
             </Tooltip>
         </Link>
     ));
