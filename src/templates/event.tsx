2cca14484ceafec7bc53849c36015044d53b6a5d
Artem K <artem.kor92@gmail.com>
2020-10-27 15:15:26 +0300

    Fix event template

--- src/templates/event.tsx
+++ src/templates/event.tsx
@@ -5,14 +5,15 @@ import Box from '@material-ui/core/Box';
 import Layout from '../components/shared/layout/layout';
 import SEO from '../components/shared/seo';
 import { CommunityEvent } from '../entities/entities';
+import { graphql } from 'gatsby';
 
 
 
-const EventPage = ({communityEvent}): ReactElement => {
+const EventPage = ({ data }): ReactElement => {
     return (
         <Layout>
             <SEO title="Event" />
-
+            {data.allMarkdownRemark.nodes[0].id}
             <Box>
             </Box>
         </Layout>
@@ -20,3 +21,28 @@ const EventPage = ({communityEvent}): ReactElement => {
 };
 
 export default EventPage;
+
+export const query = graphql`
+query ($eventId: String) {
+  allMarkdownRemark(filter: {fields: {collection: {eq: "events"}}, id: {eq: $eventId}}) {
+    nodes {
+      id
+      frontmatter {
+        name
+        date
+        speaker
+        description
+        place
+        photo {
+          childImageSharp {
+            fluid(maxWidth: 400) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stream
+      }
+    }
+  }
+}
+`;
